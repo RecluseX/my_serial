413b3fd230931bd7143d59f6a869c286809a55c5
Sirius.yang <y294107530@outlook.com>
2019-01-07 09:53:28 +0800

    add motor msg

--- src/my_serial.cpp
+++ src/my_serial.cpp
@@ -2,6 +2,8 @@
 #include <serial/serial.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Empty.h>
+#include <motor_msg/motor.h>
+#include <string.h>
 
 serial::Serial ser;
 
@@ -11,13 +13,27 @@ void write_callback(const std_msgs::String::ConstPtr& msg)
 	ser.write(msg->data);
 }
 
+
+int get_value(const char *data)
+{
+	int value = 0;
+	
+	while((*data >= '0' && *data <= '9') || *data == '\n')
+		value = value * 10 + *data++ - '0';
+	
+	return value;
+}
+
+
 int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "serial_port");
 	ros::NodeHandle n;
 	
+	
 	ros::Subscriber write_sub = n.subscribe("write", 1000, write_callback);
 	ros::Publisher  read_pub = n.advertise<std_msgs::String>("read", 1000);
+	ros::Publisher  plot_pub = n.advertise<motor_msg::motor>("motor", 1000);
 	
 	try {
 		ser.setPort("/dev/ttyUSB0");
@@ -43,9 +59,29 @@ int main(int argc, char** argv)
 		if (ser.available()) {
 	//		ROS_INFO_STREAM("Reading from serial port\n");
 			std_msgs::String result;
+		        motor_msg::motor moto;
+
 			result.data = ser.read(ser.available());
-			ROS_INFO_STREAM(result.data);
+			const char *p = result.data.c_str();	
+			const char *index1 = strstr(p, "#");	
+			const char *index2 = strstr(p, "$");
+			if (index1 != NULL) {
+				index1++;
+				moto.leftRatio = get_value(index1);
+				if (moto.leftRatio == 0)
+					ROS_INFO("left:%d", moto.leftRatio);
+			}
+			if (index2 != NULL) {
+				index2++;
+                                moto.rightRatio = 0;
+                                moto.rightRatio = get_value(index2);
+				if (moto.rightRatio == 0)
+					ROS_INFO("right:%d:", moto.rightRatio);
+			}
+			
 			read_pub.publish(result);
+			
+			plot_pub.publish(moto);
 		}
 		
 		ros::spinOnce();
